441b9ed6546e4e55fbd2422d21e58237193193a4
Joel Alftberg <joel.alftberg98@gmail.com>
2025-07-30 19:14:52 +0200

    Finishes c/difference-of-squares

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,20 @@
 #include "difference_of_squares.h"
+
+unsigned int square_of_sum(unsigned int number){
+    // formula for sum of N
+    int sum = (number * (number + 1)) / 2;
+    return sum * sum;
+    
+}
+
+unsigned int sum_of_squares(unsigned int number){
+    // formula for sum of N squared, number * (number + 1) appear in both formulas
+    return (number * (number + 1) * (2 * number + 1)) / 6;
+    
+}
+
+unsigned int difference_of_squares(unsigned int number){
+
+  return square_of_sum(number) - sum_of_squares(number);
+
+}
